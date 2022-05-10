1d6076763b150a60490c981487f62cdd5b3cc31a
Silvia Cervantes <silviaccp@gmail.com>
2022-05-10 13:21:59 -0300

    modificaciones en parrafo especialidades

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
 function muestraParrafo(parrafo){
-    this.style='opacity: 0.5';//revisar esto
-    parrafo.style='display:block;background-color:white;opacity:0.8';
+    parrafo.style='display:block;background-color:white;opacity:0.7';
 };
 function ocultaParrafo(imagen,parrafo){
     this.style='background-image:url(imagen); background-position: center; background-repeat: no-repeat;';
