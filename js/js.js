5a2299571aa8747dc0c1013eb9c78932e76a9dbe
Silvia Cervantes <silviaccp@gmail.com>
2022-05-12 13:22:08 -0300

    cambios vs

--- js/js.js
+++ js/js.js
@@ -4,4 +4,11 @@ function muestraParrafo(parrafo){
 function ocultaParrafo(imagen,parrafo){
     this.style='background-image:url(imagen); background-position: center; background-repeat: no-repeat;';
     parrafo.style='display:none';
-}
+};
+
+/* Validacion formulario */
+function validarEmail(valor) {
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor))){
+     alert("Ingrese una direccion de email valida");
+  }
+};
