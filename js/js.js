fa6bb79e9816c59805b5ab10b0acfbdb6d2c427b
Silvia Cervantes <silviaccp@gmail.com>
2022-05-13 12:05:08 -0300

    cmbios

--- js/js.js
+++ js/js.js
@@ -7,8 +7,33 @@ function ocultaParrafo(imagen,parrafo){
 };
 
 /* Validacion formulario */
-function validarEmail(valor) {
-    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor))){
-     alert("Ingrese una direccion de email valida");
-  }
+function validar_envio(){
+    if(document.f_registro.nombre.value.length==0){
+        alert("Ingrese su nombre");
+        document.f_registro.nombre.focus();
+        return 0;
+    }
+    if(document.f_registro.apellido.value.length==0){
+        alert("Ingrese su apellido");
+        document.f_registro.apellido.focus();
+        return 0;
+    }
+    if((document.f_registro.email.value.length==0) && (validarEmail(document.f_registro.email.value))){
+        alert("Ingrese su email");
+        document.f_registro.email.focus();
+        return 0;
+    }
+    if(document.f_registro.cometnario.value.length < 50){
+        alert("El comentario debe ser mayor a 50 caracteres.");
+        document.f_registro.comentario.focus();
+        return 0;
+    }
+    alert("Gracias por sus comentarios.");
+    document.f_registro.submit();
 };
+
+function validarEmail(valor) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
+     return true}else {return false}
+  }
+
